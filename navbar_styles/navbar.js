414641e0e956f81f0f825d57766b5fad4677b10b
Arun-ez <arunshaw433@gmail.com>
2023-02-04 12:25:10 +0530

    changes made

---
+++ navbar_styles/navbar.js
@@ -0,0 +1,16 @@
+
+
+function navbar() {
+    return `<div>
+            <img src="https://logos-world.net/wp-content/uploads/2022/05/Trivago-Logo-700x394.png" alt="">
+            </div>
+
+            <div id="navlist">
+            <a href=""> <i class="fa-regular fa-heart" style="font-size: 23px; opacity: 80%;"> </i> &nbsp; My favourites </a>
+            <a href=""> Log in </a>
+            <a> Menu &nbsp; <i class="fa-solid fa-chevron-down"></i> </a>
+            <a> INR &nbsp; <i class="fa-solid fa-chevron-down"></i> </a>
+            </div>`;
+}
+
+export {navbar};
